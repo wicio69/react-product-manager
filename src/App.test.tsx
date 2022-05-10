046876357866492a7f5b898c38f26de6409798ce
b-commits <s19677@pjwstk.edu.pl>
2022-05-10 13:14:24 +0200

    feat: upgrade to React 18, add basic tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from './util/store';
 import App from './App';
+import userEvent from '@testing-library/user-event';
 
 test('renders recruitment text', () => {
   const { getByText } = render(
@@ -10,4 +11,25 @@ test('renders recruitment text', () => {
     </Provider>
   );
   expect(getByText(/Bartek Gościcki - Recruitment Task/i)).toBeInTheDocument();
+});
+
+test('searchbar is initially empty', () => {
+  const { getByRole } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const searchbar = getByRole('textbox') as HTMLInputElement;
+  expect(searchbar.value).toBe('');
+});
+
+test('should be able to type in product name', () => {
+  const { getByRole } = render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
+  const searchbar = getByRole('textbox') as HTMLInputElement;
+  userEvent.type(searchbar, 'test-product');
+  expect(searchbar.value).toBe('test-product');
 });
